f04f9e41d43d811f86b52fb28de6a8c9d1c375b0
maksgeorg1-afk <maksgeorg1@gmail.com>
2026-08-26 00:32:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import os
 import asyncio
+from threading import Thread
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 TOKEN = os.getenv("BOT_TOKEN")
-
 GROUP_LINK = "https://t.me/vsedlyaludeyukraina"
 
 dp = Dispatcher()
@@ -32,7 +33,25 @@ async def start(message: Message):
     )
 
 
+class HealthHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+    def log_message(self, format, *args):
+        pass
+
+
+def run_web_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), HealthHandler)
+    server.serve_forever()
+
+
 async def main():
+    Thread(target=run_web_server, daemon=True).start()
+
     bot = Bot(token=TOKEN)
     await dp.start_polling(bot)
 
